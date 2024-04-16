2cb0eca7efc8d4c8ff10517916fe324204f4af6e
Giovanni <giovannimaldonado07@gmail.com>
2024-04-16 02:42:11 -0400

    erwthj

---
+++ API/GetUniversity.php
@@ -0,0 +1,65 @@
+
+<?php
+	// $inData = getRequestInfo();
+    $searchResults = "";
+	$searchCount = 0;
+
+    $conn = new mysqli("localhost", "Admins", "COP4710", "unigather");
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+        $sql = "SELECT Name FROM university";
+		$stmt = $conn->prepare($sql);
+		// $stmt->bind_param("si", $eventName, $eventId);
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+        while($row = $result->fetch_assoc())
+        {
+            if ($searchCount > 0){
+				$searchResults .= ",";
+			}
+			$searchCount++;
+			$searchResults .='{"name":"' . $row["Name"] . '"}';
+        }
+
+		if($searchCount == 0){
+			http_response_code(409);
+			returnWithError("No universities");
+		}
+		else{
+			http_response_code(200);
+			returnWithInfo($searchResults);
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo($searchResults)
+	{
+		$retValue = '{"university":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
